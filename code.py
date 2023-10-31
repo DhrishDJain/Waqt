5d13542efdf002240d9ef6192dfedf0e58352f53
DhrishDJain <jaindhrish12@gmail.com>
2023-10-31 19:05:01 +0530

    add feature but can you only one after running

--- code.py
+++ code.py
@@ -1,7 +1,8 @@
 from tkinter import * 
+from tkinter.font import Font
 import tkinter as tk
 
-import time
+from time import strftime
 
 class ui(tk.Tk):
     def __init__(self):
@@ -9,22 +10,78 @@ class ui(tk.Tk):
         self.geometry("500x200")
         self.title('CLOCK')
         self.iconphoto(False,tk.PhotoImage(file="icon3.png"))
-        self.maxsize(width=500,height=200)
+        self.maxsize(width=500,height=150)
         self.config(background="#616161")
     
 
 
 
-# if __name__=="__main__":
 root=ui()
 option=Frame(root,background="#616161",border=6)
 option.pack(side=LEFT,fill=Y)
+enter=Frame(root,background="#616161",border=6)
+enter.pack(anchor=CENTER,fill=X)
+ds=Font(family="Gill Sans MT",size=50)
 im=PhotoImage(file="timericon.png")
 im2=PhotoImage(file="timeicon.png")
-z=Button(option,image=im2,width=56,height=56,border=0,bg="black",activebackground="#616161")
-z.pack(side=TOP,)
-z=Button(option,image=im,width=53,height=57,border=0,background="#616161",activebackground="#616161")
-z.pack(side=TOP,pady=7)
+
+l=Label(enter,font=ds,background="#616161",foreground="#ff7f27")
+l.pack()
+
+def time1(e):
+        global hr
+        global enter
+
+        # try:
+        #      hr.destroy()
+        # except:
+        #        pass
+        # l=Label(enter,font=ds,background="#616161",foreground="#ff7f27")
+        # l.pack()
+    
+        def time():
+                String = strftime('%I:%M:%S %p')
+                l.config(text = String)
+                print("x")
+                l.after(1000,time)
+        time()
+
+
+def timer2(e):
+    global hr
+    global l
+    l.config(text="")
+    l.update()
+
+    enter.update()
+
+    
+
+    def only_numbers(char):
+        if char.isdigit() or char==" ":
+            return True
+        else:
+            return False
+
+    validation = root.register(only_numbers)
+    hr=tk.Entry(enter) 
+    hr.insert(10,"      ")
+    hr.insert(20,":")
+    hr.insert(10,"      ")
+    hr.insert(50,":")
+    hr.config(font=ds,width=13,background="#616161",border=1,foreground="#ff7f27",validate="key", validatecommand=(validation,"%S"))
+    hr.pack(side=LEFT,pady=22)
+
+
+
+clock=Button(option,image=im2,width=56,height=56,border=0,bg="black",activebackground="#616161")
+clock.bind("<Button-1>",time1)
+clock.pack(side=TOP,)
+
+timer=Button(option,image=im,width=53,height=57,border=0,background="#616161",activebackground="#616161")
+timer.bind("<Button-1>",timer2)
+timer.pack(side=TOP,pady=7)
+
         
 root.mainloop()
 
